981d276d070f480ab4d923f971b80245028bfa8b
lannoym <m.lannoy@mawi-dev.be>
2023-10-13 11:52:48 +0200

    Snake_case + Doc string

--- app/views.py
+++ app/views.py
@@ -8,22 +8,49 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 class GameManagement:
     def new_game(self, dimensions):
+     """
+        Initialize a new game board with the given dimensions.
+
+        Args:
+            dimensions (int): The width/height of the board.
+
+        Returns:
+            list: A 2D list representing the game board.
+        """
         board = [[0]*dimensions for i in range(dimensions)]
-        for rowIndex in range(dimensions):
-            for colIndex in range(dimensions):
+        for row_index in range(dimensions):
+            for col_index in range(dimensions):
                 value = 0
-                if rowIndex == 0 and colIndex == 0:
+                if row_index == 0 and col_index == 0:
                     value = 2
-                elif rowIndex == dimensions -1 and colIndex == dimensions -1:
+                elif row_index == dimensions -1 and col_index == dimensions -1:
                     value = -2
-                board[rowIndex][colIndex] = value
+                board[row_index][col_index] = value
         return board
     
     def is_game_over(self, board):
+     """
+        Check if the game is over, based on the board state.
+
+        Args:
+            board (list): The 2D list representing the game board.
+
+        Returns:
+            bool: True if the game is over, False otherwise.
+        """
         player_1_points, player_2_points = self.compute_points(board)
         return len(board) * len(board[0]) == player_1_points + player_2_points
         
     def compute_points(self, board):
+        """
+        Compute the points for each player based on the board state.
+
+        Args:
+            board (list): The 2D list representing the game board.
+
+        Returns:
+            tuple: A tuple containing the points of player 1 and player 2.
+        """
         player_1_points = 0
         player_2_points = 0
         for x in range(len(board)):
@@ -36,33 +63,51 @@ class GameManagement:
         return player_1_points, player_2_points
         
     def update_board_accoring_to_enclos(self, board):
-            rows, cols = len(board), len(board[0])
-            reachable_by_player1 = [[False] * cols for _ in range(rows)]
-            reachable_by_player2 = [[False] * cols for _ in range(rows)]
-            
-            # Find the positions of the players
-            p1i = p1j = p2i = p2j = -1
-            for i, row in enumerate(board):
-                for j, cell in enumerate(row):
-                    if cell == 2:
-                        p1i, p1j = i, j
-                    elif cell == -2:
-                        p2i, p2j = i, j
-                        
-            self.bfs(board, p1i, p1j, reachable_by_player1, 1)
-            self.bfs(board, p2i, p2j, reachable_by_player2, -1)
-            
-            for i in range(rows):
-                for j in range(cols):
-                    if board[i][j] == 0:
-                        if reachable_by_player1[i][j] and not reachable_by_player2[i][j]:
-                            board[i][j] = 1
-                        elif not reachable_by_player1[i][j] and reachable_by_player2[i][j]:
-                            board[i][j] = -1
-            return board
+        """
+        Update the board according to the game's enclosure rules.
 
+        Args:
+            board (list): The 2D list representing the game board.
+
+        Returns:
+            list: The updated board.
+        """
+        rows, cols = len(board), len(board[0])
+        reachable_by_player1 = [[False] * cols for _ in range(rows)]
+        reachable_by_player2 = [[False] * cols for _ in range(rows)]
         
+        # Find the positions of the players
+        p1i = p1j = p2i = p2j = -1
+        for i, row in enumerate(board):
+            for j, cell in enumerate(row):
+                if cell == 2:
+                    p1i, p1j = i, j
+                elif cell == -2:
+                    p2i, p2j = i, j
+                    
+        self.bfs(board, p1i, p1j, reachable_by_player1, 1)
+        self.bfs(board, p2i, p2j, reachable_by_player2, -1)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 0:
+                    if reachable_by_player1[i][j] and not reachable_by_player2[i][j]:
+                        board[i][j] = 1
+                    elif not reachable_by_player1[i][j] and reachable_by_player2[i][j]:
+                        board[i][j] = -1
+        return board
+
     def bfs(self, board, i, j, reachable, player):
+        """
+        Breadth-First Search to explore reachable positions for a player on the board.
+
+        Args:
+            board (list): The 2D list representing the game board.
+            i (int): Starting row index for BFS.
+            j (int): Starting column index for BFS.
+            reachable (list): 2D boolean list indicating whether a cell is reachable.
+            player (int): The player to check reachability for.
+        """
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         queue = deque([(i, j)])
         
@@ -81,7 +126,12 @@ class GameManagement:
 # Waiting database to be ready to remove session variable use
 @app.route('/loadGame')
 def loadGame():
-    "return index template"
+    """
+    Initialize a new game session and respond with the initial game state.
+
+    Returns:
+        JSON: JSON data containing the initial game state.
+    """
     session['game'] = {
         "board": GameManagement().new_game(10),
         "activePlayer": 1,
@@ -92,18 +142,36 @@ def loadGame():
 
 @app.route('/game')
 def game():
+    """
+    Render the game HTML template.
+
+    Returns:
+        HTML: Rendered HTML for the game.
+    """
     "return index template"
     return render_template('game.html')
 
     
 @app.route('/')
 def index():
+    """
+    Render the index HTML template.
+
+    Returns:
+        HTML: Rendered HTML for the index page.
+    """
     "return index template"
     return render_template('index.html')
 
 
 @app.route('/move')
 def move():
+    """
+    Process a player's move, update the game state, and respond with the new state.
+
+    Returns:
+        JSON/str: Updated game state in JSON format or an error message string.
+    """
     direction = request.args['direction']
     player = int(request.args['player'])
     game = session["game"]
